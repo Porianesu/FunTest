c44f8db20b76460c1cac0f4f4eb88077cb99445f
Porianesu <976384615@qq.com>
2024-11-21 11:51:18 +0800

    feat():1.Some changes

--- src/NodeTest/test.js
+++ src/NodeTest/test.js
@@ -1,49 +1,75 @@
-console.log("start");
-setTimeout(() => {
-  console.log("setImmediate");
-}, 0);
-setInterval(() => {
-  console.log("setInterval");
-}, 1000);
-setTimeout(() => {
-  console.log("timeout2");
-  new Promise((resolve, reject) => {
-    resolve();
-    console.log("timeout2_promise");
-  }).then(() => {
-    console.log("timeout2_then");
-  });
-}, 1000);
-for (let i = 1; i < 6; i++) {
-  setTimeout(() => {
-    console.log("loop" + i);
-    console.log(new Date().getTime());
-  }, i * 1000);
-  console.log(i);
-}
-setTimeout(() => {
-  console.log("timeout1");
-  new Promise((resolve, reject) => {
-    resolve();
-    console.log("timeout1_promise");
-  }).then(() => {
-    console.log("timeout1_then");
-  });
-}, 2000);
-async function xhr() {
-  console.log("xhr");
-  return "async";
-}
-async function asyncFn() {
-  console.log("asyncFn");
-  const tag = await xhr();
-  return tag;
-}
+// 变量提升与函数提升
+// console.log(v1);
+// var v1 = 100;
+// function foo() {
+//   console.log(v1);
+//   var v1 = 200;
+//   console.log(v1);
+// }
+// foo();
+// console.log(v1);
 
-asyncFn().then((res) => console.log("asyncFn().then", res));
-new Promise((resolve, reject) => {
-  console.log("promise1");
-  reject();
-}).catch(() => {
-  console.log("catch1");
-});
+// console.log("start");
+// setTimeout(() => {
+//   console.log("setImmediate");
+// }, 0);
+// setInterval(() => {
+//   console.log("setInterval");
+// }, 1000);
+// setTimeout(() => {
+//   console.log("timeout2");
+//   new Promise((resolve, reject) => {
+//     resolve();
+//     console.log("timeout2_promise");
+//   }).then(() => {
+//     console.log("timeout2_then");
+//   });
+// }, 1000);
+// for (let i = 1; i < 6; i++) {
+//   setTimeout(() => {
+//     console.log("loop" + i);
+//     console.log(new Date().getTime());
+//   }, i * 1000);
+//   console.log(i);
+// }
+// setTimeout(() => {
+//   console.log("timeout1");
+//   new Promise((resolve, reject) => {
+//     resolve();
+//     console.log("timeout1_promise");
+//   }).then(() => {
+//     console.log("timeout1_then");
+//   });
+// }, 2000);
+// async function xhr() {
+//   console.log("xhr");
+//   return "async";
+// }
+// async function asyncFn() {
+//   console.log("asyncFn");
+//   const tag = await xhr();
+//   return tag;
+// }
+//
+// asyncFn().then((res) => console.log("asyncFn().then", res));
+// new Promise((resolve, reject) => {
+//   console.log("promise1");
+//   reject();
+// }).catch(() => {
+//   console.log("catch1");
+// });
+const B = () => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve();
+    }, 1000);
+  }).then(() => {
+    console.log("B");
+  });
+};
+const A = async () => {
+  console.log("A start");
+  await B();
+  console.log("A end");
+};
+A();
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "@/App";
-// import BuildYourOwnReactApp from "@/BuildYourOwnReact/BuildYourOwnReactApp";
+import BuildYourOwnReactApp from "@/BuildYourOwnReact/BuildYourOwnReactApp";
 import reportWebVitals from "@/reportWebVitals";
 import { Route, Routes } from "react-router";
 import { BrowserRouter } from "react-router-dom";
@@ -13,44 +13,44 @@ import Expenses from "Routes/Expenses";
 import Invoices from "Routes/Invoices";
 import Invoice from "Routes/Invoice";
 /** @tsxRuntime classic */
-// import Didactic from "./BuildYourOwnReact/Didactic";
-// const element = <BuildYourOwnReactApp name={"good 123"} />;
+import Didactic from "./BuildYourOwnReact/Didactic";
+const element = <BuildYourOwnReactApp name={"good 123"} />;
 const container = document.getElementById("root");
-// Didactic.render(element, container);
-ReactDOM.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path={"/"} element={<App />}>
-          <Route path="expenses" element={<Expenses />} />
-          <Route path="invoices" element={<Invoices />}>
-            <Route
-              index
-              element={
-                <main style={{ padding: "1rem" }}>
-                  <p>Select an invoice</p>
-                </main>
-              }
-            />
-            <Route path=":invoiceId" element={<Invoice />} />
-          </Route>
-          <Route
-            path="*"
-            element={
-              <main style={{ padding: "1rem" }}>
-                <p>There's nothing here!</p>
-              </main>
-            }
-          />
-          <Route path={"matterJsDemo"} element={<MatterJsDemo />} />
-          <Route path={"imageDemo"} element={<ImageDemo />} />
-        </Route>
-        <Route path={"threeJsDemo"} element={<ThreeJsDemo />} />
-      </Routes>
-    </BrowserRouter>
-  </React.StrictMode>,
-  container
-);
+Didactic.render(element, container);
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <BrowserRouter>
+//       <Routes>
+//         <Route path={"/"} element={<App />}>
+//           <Route path="expenses" element={<Expenses />} />
+//           <Route path="invoices" element={<Invoices />}>
+//             <Route
+//               index
+//               element={
+//                 <main style={{ padding: "1rem" }}>
+//                   <p>Select an invoice</p>
+//                 </main>
+//               }
+//             />
+//             <Route path=":invoiceId" element={<Invoice />} />
+//           </Route>
+//           <Route
+//             path="*"
+//             element={
+//               <main style={{ padding: "1rem" }}>
+//                 <p>There's nothing here!</p>
+//               </main>
+//             }
+//           />
+//           <Route path={"matterJsDemo"} element={<MatterJsDemo />} />
+//           <Route path={"imageDemo"} element={<ImageDemo />} />
+//         </Route>
+//         <Route path={"threeJsDemo"} element={<ThreeJsDemo />} />
+//       </Routes>
+//     </BrowserRouter>
+//   </React.StrictMode>,
+//   container
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
